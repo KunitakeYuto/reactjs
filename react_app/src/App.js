9e1b6cde1da323b75c02f8cd1b1efeea89fe612d
KunitakeYuto <k.yut.online@gmail.com>
2020-12-05 16:52:40 +0900

    3

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,48 +1,19 @@
 import logo from './logo.svg';
 import React, { Component } from "react";
 import './App.css';
+import Rect from "./Rect"
 //import { Component } from 'react';
 
 class App extends Component {
   render() {
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="cyan" />
-      <Rect x="150" y="100" w="150" h="150" c="magenta" />
-      <Rect x="100" y="150" w="150" h="150" c="black" />
+      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" r="50" />
+      <Rect x="100" y="150" w="150" h="150" c="black" r="50" />
     </div >;
   }
 }
 
-class Rect extends Component {
-  x = 0;
-  y = 0;
-  width = 0;
-  height = 0;
-  color = "white"
-  style = {};
-
-  constructor(props) {
-    super(props);
-    this.x = props.x;
-    this.y = props.y;
-    this.width = props.width;
-    this.height = props.height;
-    this.color = props.c;
-
-    this.style = {
-      backgroundColor: this.color,
-      position: "absolute",
-      left: this.x + "px",
-      top: this.y + "px",
-      width: this.width + "px",
-      height: this.height + "px"
-    }
-  }
-  render() {
-    return <div style={this.style}></div>
-  }
-
-}
 
 export default App;
