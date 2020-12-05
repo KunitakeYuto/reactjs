e2dc2dea010841fc7aa03e7e4c5fafa5b2940523
KunitakeYuto <k.yut.online@gmail.com>
2020-12-05 18:05:54 +0900

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -20,21 +20,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      msg: "Hello",
+      counter: 0,
+      msg: "count start!",
     };
-    let timer = setInterval(() => {
-      this.setState((state) => ({
-        msg: state.msg + "!"
-      }));
-    }, 10000);
+    this.doAction = this.doAction.bind(this);
+  }
 
+  doAction(e) {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: "count" + state.counter
+    }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
       <p style={this.msgStyle}>{this.state.msg}</p>
-      <p style={this.msgStyle}>{this.props.msg}</p>
+      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
 }
