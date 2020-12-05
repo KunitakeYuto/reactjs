cabc5e13a60bcf8182d85ee8e2b31ade4be8a63c
KunitakeYuto <k.yut.online@gmail.com>
2020-12-05 16:27:24 +0900

    2

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,14 +1,48 @@
 import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+//import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  render() {
+    return <div>
       <h1>React</h1>
-      <p>This is sample component</p>
-      <p>これはサンプルコンポーネントです</p>
-    </div>
-  );
+      <Rect x="50" y="50" w="150" h="150" c="cyan" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" />
+      <Rect x="100" y="150" w="150" h="150" c="black" />
+    </div >;
+  }
+}
+
+class Rect extends Component {
+  x = 0;
+  y = 0;
+  width = 0;
+  height = 0;
+  color = "white"
+  style = {};
+
+  constructor(props) {
+    super(props);
+    this.x = props.x;
+    this.y = props.y;
+    this.width = props.width;
+    this.height = props.height;
+    this.color = props.c;
+
+    this.style = {
+      backgroundColor: this.color,
+      position: "absolute",
+      left: this.x + "px",
+      top: this.y + "px",
+      width: this.width + "px",
+      height: this.height + "px"
+    }
+  }
+  render() {
+    return <div style={this.style}></div>
+  }
+
 }
 
 export default App;
