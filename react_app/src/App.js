5819ac71727551a3917ba6312ed5cd7c5707d29a
KunitakeYuto <k.yut.online@gmail.com>
2020-12-07 20:17:15 +0900

    flg

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,13 +5,23 @@ import Rect from "./Rect"
 //import { Component } from 'react';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize: "24pt",
     color: "#900",
     margin: "20px 0px",
     padding: "5px",
     borderBottom: "2px solid #900"
   }
+
+  msgStyle2 = {
+    fontSize: "20pt",
+    color: "white",
+    backgroundColor: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+
   btnStyle = {
     fontSize: "20pt",
     padding: "0px 10px"
@@ -29,16 +39,21 @@ class App extends Component {
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: "count" + state.counter
+      msg: state.counter,
+      flg: !state.flg,
     }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>count:{this.state.msg}</p>
+        :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
-    </div>;
+    </div>
   }
 }
 
