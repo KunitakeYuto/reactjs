e99ba4482e67afcc8ecf1f9ceafe76e1583069fd
KunitakeYuto <k.yut.online@gmail.com>
2020-12-09 10:34:49 +0900

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,61 +1,72 @@
-import logo from './logo.svg';
 import React, { Component } from "react";
 import './App.css';
 import Rect from "./Rect"
 //import { Component } from 'react';
 
 class App extends Component {
-  data = [];
-
+  input = "";
   msgStyle = {
-    fontSize: "2opt",
+    fontSize: "20pt",
     color: "#900",
     margin: "20px 0px",
-    padding: "5px",
-  }
-
-  area = {
-    width: "500px",
-    height: "500px",
-    border: "1px solid blue"
+    padding: "5px"
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      list: this.data,
-    };
-    this.doAction = this.doAction.bind(this);
+      message: "type your name",
+    }
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
   }
 
-  doAction(e) {
-    let x = e.pageX;
-    let y = e.pageY;
-    this.data.push({ x: x, y: y });
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
     this.setState({
-      list: this.data
+      message: "Hello" + this.input + "!"
     });
-  }
-
-  draw(d) {
-    let s = {
-      position: "absolute",
-      left: (d.x - 25) + "px",
-      top: (d.y - 25) + "px",
-      width: "50px",
-      height: "50px",
-      backgroundColor: "#66f3"
-    };
-    return <div style={s}></div>
+    event.preventDefault();
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <h2 style={this.msgStyle}>shiw react.</h2>
-      <div style={this.area} onClick={this.doAction}>
-        {this.data.map((value) => this.draw(value))}
-      </div>
+      <Message title="Children">
+        これはコンポーネント内のコンテンツです。
+        まるでテキストを分割し、リストにして表示します。
+        改行は必要ありません。
+      </Message>
+    </div>
+  }
+}
+
+class Message extends Component {
+  li = {
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "0px",
+  }
+
+  render() {
+    let content = this.props.children;
+    let arr = content.split("。");
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].trim() != "") {
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value, key) => (
+      <li style={this.li} key={key}>{value}</li>
+    ));
+    return <div>
+      <h2>{this.props.title}</h2>
+      <ol>{list}</ol>
     </div>
   }
 }
