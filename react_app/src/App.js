92fec3a1c08bde72d9068cb72137bc7401eca6d4
KunitakeYuto <k.yut.online@gmail.com>
2020-12-15 18:29:13 +0900

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import './App.css';
-import Rect from "./Rect"
 //import { Component } from 'react';
 
 class App extends Component {
   input = "";
+
+
   msgStyle = {
     fontSize: "20pt",
     color: "#900",
@@ -17,12 +18,11 @@ class App extends Component {
     this.state = {
       message: "type your name",
     }
-    this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
+    this.doCheck = this.doChange.bind(this);
   }
 
   doChange(event) {
-    this.input = event.target.value;
+    alert(event.target.value + "は長すぎます(10文字まで)");
   }
 
   doSubmit(event) {
@@ -35,41 +35,34 @@ class App extends Component {
   render() {
     return <div>
       <h1>React</h1>
-      <Message title="Children">
-        これはコンポーネント内のコンテンツです。
-        まるでテキストを分割し、リストにして表示します。
-        改行は必要ありません。
-      </Message>
-    </div>
+      <h2>{this.state.message}</h2>
+      <Message maxlength="10" onCheck={this.doCheck}></Message>
+    </div>;
   }
 }
 
 class Message extends Component {
-  li = {
-    fontSize: "16pt",
-    color: "#06",
-    margin: "0px",
-    padding: "0px",
+  inputStyle = {
+    fontSize: "12pt",
+    padding: "5px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.doChange = this.doChange.bind(this);
+  }
+
+  doChange(e) {
+    if (e.target.value.length > this.props.maxlength) {
+      this.props.onCheck(e);
+      e.target.valve = e.target.value.substr(0, this.props.maxlength);
+    }
   }
 
   render() {
-    let content = this.props.children;
-    let arr = content.split("。");
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].trim() != "") {
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value, key) => (
-      <li style={this.li} key={key}>{value}</li>
-    ));
-    return <div>
-      <h2>{this.props.title}</h2>
-      <ol>{list}</ol>
-    </div>
+    return <input type="text" style={this.inputStyle} onChange={this.doChange} />
+
   }
 }
-
 
 export default App;
