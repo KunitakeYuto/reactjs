a5f3eef39580b22b1dff6601c0489cdb99afe6c9
KunitakeYuto <k.yut.online@gmail.com>
2020-12-18 17:08:44 +0900

    a

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
+
 //import reportWebVitals from './reportWebVitals';
+
+let state_value = {
+  counter: 0,
+  message: "COUNTER"
+}
+
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT"
+      };
+      defaut:
+      return state;
+  }
+}
 
 ReactDOM.render(
   <App msg="Hello App" />,
