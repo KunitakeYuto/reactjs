4c94bc6d839c2c5041129ffe55499c41f96fbc4a
KunitakeYuto <k.yut.online@gmail.com>
2020-12-19 16:49:51 +0900

    1219

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,68 +1,74 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import './App.css';
 //import { Component } from 'react';
 
+function mappingState(state) {
+  return state;
+}
+
 class App extends Component {
-  input = "";
-
-
-  msgStyle = {
-    fontSize: "20pt",
-    color: "#900",
-    margin: "20px 0px",
-    padding: "5px"
-  }
-
   constructor(props) {
     super(props);
-    this.state = {
-      message: "type your name",
-    }
-    this.doCheck = this.doChange.bind(this);
-  }
-
-  doChange(event) {
-    alert(event.target.value + "は長すぎます(10文字まで)");
-  }
-
-  doSubmit(event) {
-    this.setState({
-      message: "Hello" + this.input + "!"
-    });
-    event.preventDefault();
   }
 
   render() {
-    return <div>
-      <h1>React</h1>
-      <h2>{this.state.message}</h2>
-      <Message maxlength="10" onCheck={this.doCheck}></Message>
-    </div>;
+    return (
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <button />
+      </div>
+    )
   }
 }
 
+App = connect()(App);
 class Message extends Component {
-  inputStyle = {
-    fontSize: "12pt",
-    padding: "5px",
+  style = {
+    fontSize: "20px",
+    padding: "20px 5px"
   }
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}:{this.props.counter}
+      </p>
+    );
+  }
+}
 
+Message = connect(mappingState)(Message);
+
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px"
+  }
   constructor(props) {
     super(props);
-    this.doChange = this.doChange.bind(this);
+    this.doAction = this.doAction.bind(this);
   }
 
-  doChange(e) {
-    if (e.target.value.length > this.props.maxlength) {
-      this.props.onCheck(e);
-      e.target.valve = e.target.value.substr(0, this.props.maxlength);
+  //クリック
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type: "DECREMENT" });
+    } else {
+      this.props.dispatch({ type: "INCREMENT" });
     }
   }
 
   render() {
-    return <input type="text" style={this.inputStyle} onChange={this.doChange} />
-
+    return (
+      <button style={this.style}
+        onClick={this.doAction}>
+        click
+      </button>
+    );
   }
 }
+
+Button = connect()(Button);
 
 export default App;
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';//react-reduxの機能
+
 import './index.css';
 import App from './App';
 
@@ -24,12 +26,10 @@ function counter(state = state_value, action) {
   }
 }
 
+let store = createStore(counter);
 ReactDOM.render(
-  <App msg="Hello App" />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider >,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
