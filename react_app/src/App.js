6bf06fb5bb24aad7e503ee73abe9b67e8e467069
KunitakeYuto <k.yut.online@gmail.com>
2020-12-20 15:38:52 +0900

    1220

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,74 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import './App.css';
-//import { Component } from 'react';
-
-function mappingState(state) {
-  return state;
-}
+import Memo from "./memo/Memo";
+import AddForm from "./memo/AddForm";
+import FindForm from "./memo/FindForm";
+import DelForm from "./memo/DelForm";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  td = {
+    width: "250"
   }
-
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <button />
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table><tbody><tr>
+          <td style={this.td}><FindForm /></td>
+          <td style={this.td}><DelForm /></td>
+        </tr></tbody></table>
+        <Memo />
       </div>
-    )
-  }
-}
-
-App = connect()(App);
-class Message extends Component {
-  style = {
-    fontSize: "20px",
-    padding: "20px 5px"
-  }
-  render() {
-    return (
-      <p style={this.style}>
-        {this.props.message}:{this.props.counter}
-      </p>
     );
   }
 }
 
-Message = connect(mappingState)(Message);
 
-class Button extends Component {
-  style = {
-    fontSize: "16pt",
-    padding: "5px 10px"
-  }
-  constructor(props) {
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  //クリック
-  doAction(e) {
-    if (e.shiftKey) {
-      this.props.dispatch({ type: "DECREMENT" });
-    } else {
-      this.props.dispatch({ type: "INCREMENT" });
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style}
-        onClick={this.doAction}>
-        click
-      </button>
-    );
-  }
-}
-
-Button = connect()(Button);
 
 export default App;
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { createStore, combineReducers } from "redux";
 import { Provider } from 'react-redux';//react-reduxの機能
-
-import './index.css';
+import "./index.css";
 import App from './App';
+import MemoStore from "./memo/Store";
 
 //import reportWebVitals from './reportWebVitals';
-
-let state_value = {
-  counter: 0,
-  message: "COUNTER"
-}
-
-function counter(state = state_value, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-        message: "INCREMENT"
-      };
-      defaut:
-      return state;
-  }
-}
-
-let store = createStore(counter);
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={MemoStore}>
     <App />
-  </Provider >,
+  </Provider>,
   document.getElementById("root")
 );
+
