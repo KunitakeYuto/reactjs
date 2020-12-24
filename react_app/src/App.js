963dc297b3322612bae9ae5f91bdfb67388ea25a
KunitakeYuto <k.yut.online@gmail.com>
2020-12-24 16:18:47 +0900

    redux

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,38 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/AddForm';
-import FindForm from './memo/FindForm';
-import DelForm from './memo/DelForm';
 
-
-// Appコンポーネント
 class App extends Component {
-  td = {
-    width: "250px"
-  }
-
-
   constructor(props) {
     super(props);
   }
-
   render() {
     return (
       <div>
-        <h1>Memo</h1>
-        <AddForm />
-        <hr />
-        <table><tbody><tr>
-          <td style={this.td}><FindForm /></td>
-          <td style={this.td}><DelForm /></td>
-        </tr></tbody></table>
-        <Memo />
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>
     );
   }
 }
 
+App = connect()(App);
 
-export default connect()(App);
+class Message extends Component {
+  style = {
+    fontSize: "20pt",
+    padding: "20px 5px"
+  }
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}:{this.props.counter}
+      </p>
+    );
+  }
+}
+
+Message = connect((state) => (Message));
+
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px"
+  }
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type: "DECREMENT" });
+    } else if (e.ctrlKey) {
+      this.props.dispatch({ type: "RESET" });
+    } else {
+      this.props.dispatch({ type: "INCREMENT" });
+    }
+  }
+
+  render() {
+    return (
+      <button style={this.style} onClick={this.doAction}>
+        click
+      </button>
+    );
+  }
+}
+
+Button = connect()(Button);
+export default App;
