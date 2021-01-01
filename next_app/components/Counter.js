222fa2388ebb2cda2788e4166b818e81b9f27939
KunitakeYuto <k.yut.online@gmail.com>
2021-01-01 16:41:11 +0900

    01011641

--- next_app/components/Counter.js
+++ next_app/components/Counter.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Counter extends Component {
     style = {
         fontSize: "12pt",
-        padding: "5px 15px",
+        padding: "5px 15px"
     }
+
 
     constructor(props) {
         super(props);
@@ -13,29 +15,34 @@ class Counter extends Component {
         this.reset = this.reset.bind(this);
     }
 
+
     doAction(e) {
         if (e.shiftKey) {
-            return this.props.dispatch({ type: "DECREMENT" });
+            return this.props.dispatch({ type: 'DECREMENT' });
         } else {
-            return this.props.dispatch({ type: "INCREMENT" });
+            return this.props.dispatch({ type: 'INCREMENT' });
         }
     }
 
+
     reset() {
-        return this.props.dispatch({ type: "RESET" });
+        return this.props.dispatch({ type: 'RESET' });
     }
+
 
     render() {
         return (
             <div>
-                <p>{this.props.message}:{this.props.count}</p>
-                <button style={this.style} onClick={this.doAction}>Count</button>
-                <button style={this.style} onClick={this.reset}>Reset</button>
+                <p>{this.props.message}: {this.props.count}</p>
+                <button style={this.style} onClick={this.doAction}>
+                    Count</button>
+                <button style={this.style} onClick={this.reset}>
+                    Reset</button>
             </div>
         )
     }
 }
 
+
 Counter = connect((state) => state)(Counter);
 export default Counter;
-
