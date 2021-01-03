0935f24a0b22396e6e0571644a0c0ff85f6baa1a
KunitakeYuto <k.yut.online@gmail.com>
2021-01-03 14:13:34 +0900

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,40 +1,31 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/AddForm';
-import FindForm from './memo/FindForm';
-import DelForm from './memo/DelForm';
-import PersistForm from "./memo/PersistForm";
+import "./App.css";
+import Sampledata from "./fire/SampleData";
+import firebase from "firebase";
 
+var config = {
+  apiKey: "AIzaSyAvTQeelJrzkKAv375BIA-MSo_lHDAR0QQ",
+  authDomain: "react-gakusyu.firebaseapp.com",
+  databaseURL: "https://react-gakusyu-default-rtdb.firebaseio.com",
+  projectId: "react-gakusyu",
+  storageBucket: "react-gakusyu.appspot.com",
+  messagingSenderId: "22479159306",
+  appId: "1:22479159306:web:88b323adafb2511f4a1ff3",
+  measurementId: "G-0EHV7VTWH6"
+};
 
-// Appコンポーネント
+firebase.initializeApp(config);
+
 class App extends Component {
-  td = {
-    width: "250px"
-  }
-
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
-        <h1>Memo</h1>
-        <AddForm />
-        <hr />
-        <table><tbody><tr>
-          <td style={this.td}><FindForm /></td>
-          <td style={this.td}><DelForm /></td>
-          <td style={this.td}><PersistForm /></td>
-        </tr></tbody></table>
-        <Memo />
+        <h1>Fire</h1>
+        <h2>Sample data.</h2>
+        <Sampledata />
       </div>
-    );
+    )
   }
 }
 
-
-export default connect()(App);
+export default App;
